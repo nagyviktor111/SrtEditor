6825e5fdc0a16f5e5b965200feffc3dadddaae48
viktor nagy <nagyviktor1111@outlook.com>
2024-10-14 21:31:59 +0200

    renamer init

---
+++ SrtEditor.Domain/Renamer/RenamerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace SrtEditor.Domain.Renamer
+{
+    public class RenamerOptions
+    {
+        public string? FolderPath { get; set; }
+
+        public bool CopyVideoNames { get; set; }
+
+        public bool ExtendFileNames { get; set; }
+
+        public string? Extension { get; set; }
+    }
+}
